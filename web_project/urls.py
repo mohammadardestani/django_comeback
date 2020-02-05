bc7a8d273ddb9e3368abde8146ebbbf83a3d393f
mohammadjavadArdestani <mjavad.ardestani00@gmail.com>
2020-02-05 19:52:03 +0330

    template folder added

--- web_project/urls.py
+++ web_project/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from . import viwes
 urlpatterns = [
+    path( '',viwes.home_page),
     path('admin/', admin.site.urls),
 ]
